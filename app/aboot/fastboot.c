3575097d4c37c9f22c8f8d6269e335eb7a02d75d
M1cha <sigmaepsilon92@gmail.com>
2014-04-27 22:53:45 +0200

    app: fastboot: implement command 'fastboot getvar all'
    (cherry picked from commit caa465f718607b1c2de7620baa106159fd39a096)

--- app/aboot/fastboot.c
+++ app/aboot/fastboot.c
@@ -450,6 +450,10 @@ static void getvar_all()
 static void cmd_getvar(const char *arg, void *data, unsigned sz)
 {
 	struct fastboot_var *var;
+	bool all = false;
+	char response[128];
+
+	all = !strcmp("all", arg);
 
 #if CHECK_BAT_VOLTAGE
 	update_battery_status();
@@ -463,7 +467,11 @@ static void cmd_getvar(const char *arg, void *data, unsigned sz)
 	}
 
 	for (var = varlist; var; var = var->next) {
-		if (!strcmp(var->name, arg)) {
+		if (all) {
+			snprintf(response, sizeof(response), "\t%s: [%s]", var->name, var->value);
+			fastboot_info(response);
+		}
+		else if (!strcmp(var->name, arg)) {
 			fastboot_okay(var->value);
 			return;
 		}
