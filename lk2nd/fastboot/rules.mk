17fc75a3f83c168b441a704d81b9b206c6feb754
Stephan Gerhold <stephan@gerhold.net>
2021-03-17 15:29:33 +0100

    lk2nd: fastboot: Add "fastboot oem screenshot"
    Add a simple way to capture the screen contents in LK using a fastboot
    command. The implementation converts from different color formats
    (RGB565, RGB888, RGB8888) using NEON assembly (inspired from examples
    in the ARM documentation) and outputs an image using the simple PPM
    format.
    
    Usage: "fastboot oem screenshot && fastboot get_staged screen.ppm"
    
    (cherry picked from commit ac37f4465433b740b6e31a783dba771d91e2d431)
    [move to lk2nd/fastboot, extend NEON assembly to handle RGB565/RGB8888]
    Co-Authored-By: Nikita Travkin <nikita@trvn.ru>

--- lk2nd/fastboot/rules.mk
+++ lk2nd/fastboot/rules.mk
@@ -3,3 +3,9 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 OBJS += \
 	$(LOCAL_DIR)/fetch.o \
 	$(LOCAL_DIR)/misc.o \
+
+ifneq ($(filter DISPLAY_SPLASH_SCREEN=1,$(DEFINES)),)
+OBJS += \
+	$(LOCAL_DIR)/screenshot.o \
+	$(LOCAL_DIR)/screenshot-neon.o
+endif
