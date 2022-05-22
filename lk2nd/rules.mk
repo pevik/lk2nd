60f49374692e5b08ca60fd3516c88113923658b5
Stephan Gerhold <stephan@gerhold.net>
2022-05-22 14:05:37 +0200

    lk2nd: Add initial lk2nd module
    This directory/module will contain functionality specific to the lk2nd
    project. It uses the GPL-2.0-only license but is only enabled optionally,
    i.e. the rest of LK can still be used under the original license.

---
+++ lk2nd/rules.mk
@@ -0,0 +1,19 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+INCLUDES += -I$(LOCAL_DIR)/include
+
+GPL ?= 0
+DEFINES += GPL=$(GPL)
+ifeq ($(GPL), 1)
+BUILD_GPL := 1
+ifeq ($(CRYPTO_BACKEND), openssl)
+$(warning WARNING: OpenSSL License is not compatible with \
+	  GPL-2.0-only used for some parts of the lk2nd module. \
+	  Set GPL=0 to exclude GPL-licensed code.)
+endif
+ifeq ($(DTBO_BACKEND), libufdt)
+$(warning WARNING: Apache-2.0 license of libufdt is not compatible with \
+	  GPL-2.0-only used for some parts of the lk2nd module. \
+	  Set GPL=0 to exclude GPL-licensed code.)
+endif
+endif
