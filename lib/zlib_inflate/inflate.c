f7d3fe60c43267724e2e6bd89718fc42c66e76aa
Petr Vorel <petr.vorel@gmail.com>
2022-07-08 03:08:54 +0200

    zlib_inflate/inflate: Return Z_STREAM_ERROR
    lib/zlib_inflate/inflate.c: In function 'inflateMark':
    lib/zlib_inflate/inflate.c:1523:61: error: left shift of negative value [-Werror=shift-negative-value]
     1523 |     if (strm == Z_NULL || strm->state == Z_NULL) return -1L << 16;
          |                                                             ^~
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- lib/zlib_inflate/inflate.c
+++ lib/zlib_inflate/inflate.c
@@ -1520,7 +1520,8 @@ z_streamp strm;
 {
     struct inflate_state FAR *state;
 
-    if (strm == Z_NULL || strm->state == Z_NULL) return -1L << 16;
+    if (strm == Z_NULL || strm->state == Z_NULL)
+        return Z_STREAM_ERROR;
     state = (struct inflate_state FAR *)strm->state;
     return ((long)(state->back) << 16) +
         (state->mode == COPY ? state->length :
